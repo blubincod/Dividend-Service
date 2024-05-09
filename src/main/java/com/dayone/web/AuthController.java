71f778a5447b68f0f2a3a39a58e6f96330af8084
JYP <bluebinarycode@gmail.com>
2024-05-09 13:09:01 +0900

    DividendService_v0.1

---
+++ src/main/java/com/dayone/web/AuthController.java
@@ -0,0 +1,30 @@
+package com.dayone.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+//    private final MemberService memberService;
+
+//    @PostMapping("/signup")
+//    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+//        var result = this.memberService.register(request);
+//        return ResponseEntity.ok(result);
+//    }
+//
+//    @PostMapping("/signin")
+//    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request) {
+//        var member = this.memberService.authenticate(request);
+//        var token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+//        log.info("user login -> " + request.getUsername());
+//        return ResponseEntity.ok(token);
+//    }
+
+}
